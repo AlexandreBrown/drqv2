5e79651f48fb3a3775da05d2eb7fb563669eb822
AlexandreBrown <alexandrebrown97@gmail.com>
2024-11-04 13:16:38 -0500

    linter refactor

--- src/drqv2/agent.py
+++ src/drqv2/agent.py
@@ -20,29 +20,22 @@ class RandomShiftsAug(nn.Module):
         n, c, h, w = x.size()
         assert h == w
         padding = tuple([self.pad] * 4)
-        x = F.pad(x, padding, 'replicate')
+        x = F.pad(x, padding, "replicate")
         eps = 1.0 / (h + 2 * self.pad)
-        arange = torch.linspace(-1.0 + eps,
-                                1.0 - eps,
-                                h + 2 * self.pad,
-                                device=x.device,
-                                dtype=x.dtype)[:h]
+        arange = torch.linspace(
+            -1.0 + eps, 1.0 - eps, h + 2 * self.pad, device=x.device, dtype=x.dtype
+        )[:h]
         arange = arange.unsqueeze(0).repeat(h, 1).unsqueeze(2)
         base_grid = torch.cat([arange, arange.transpose(1, 0)], dim=2)
         base_grid = base_grid.unsqueeze(0).repeat(n, 1, 1, 1)
 
-        shift = torch.randint(0,
-                              2 * self.pad + 1,
-                              size=(n, 1, 1, 2),
-                              device=x.device,
-                              dtype=x.dtype)
+        shift = torch.randint(
+            0, 2 * self.pad + 1, size=(n, 1, 1, 2), device=x.device, dtype=x.dtype
+        )
         shift *= 2.0 / (h + 2 * self.pad)
 
         grid = base_grid + shift
-        return F.grid_sample(x,
-                             grid,
-                             padding_mode='zeros',
-                             align_corners=False)
+        return F.grid_sample(x, grid, padding_mode="zeros", align_corners=False)
 
 
 class Encoder(nn.Module):
@@ -52,11 +45,16 @@ class Encoder(nn.Module):
         assert len(obs_shape) == 3
         self.repr_dim = 32 * 35 * 35
 
-        self.convnet = nn.Sequential(nn.Conv2d(obs_shape[0], 32, 3, stride=2),
-                                     nn.ReLU(), nn.Conv2d(32, 32, 3, stride=1),
-                                     nn.ReLU(), nn.Conv2d(32, 32, 3, stride=1),
-                                     nn.ReLU(), nn.Conv2d(32, 32, 3, stride=1),
-                                     nn.ReLU())
+        self.convnet = nn.Sequential(
+            nn.Conv2d(obs_shape[0], 32, 3, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, stride=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, stride=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, stride=1),
+            nn.ReLU(),
+        )
 
         self.apply(utils.weight_init)
 
@@ -71,14 +69,17 @@ class Actor(nn.Module):
     def __init__(self, repr_dim, action_dim, feature_dim, hidden_dim):
         super().__init__()
 
-        self.trunk = nn.Sequential(nn.Linear(repr_dim, feature_dim),
-                                   nn.LayerNorm(feature_dim), nn.Tanh())
+        self.trunk = nn.Sequential(
+            nn.Linear(repr_dim, feature_dim), nn.LayerNorm(feature_dim), nn.Tanh()
+        )
 
-        self.policy = nn.Sequential(nn.Linear(feature_dim, hidden_dim),
-                                    nn.ReLU(inplace=True),
-                                    nn.Linear(hidden_dim, hidden_dim),
-                                    nn.ReLU(inplace=True),
-                                    nn.Linear(hidden_dim, action_dim))
+        self.policy = nn.Sequential(
+            nn.Linear(feature_dim, hidden_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_dim, action_dim),
+        )
 
         self.apply(utils.weight_init)
 
@@ -97,18 +98,25 @@ class Critic(nn.Module):
     def __init__(self, repr_dim, action_dim, feature_dim, hidden_dim):
         super().__init__()
 
-        self.trunk = nn.Sequential(nn.Linear(repr_dim, feature_dim),
-                                   nn.LayerNorm(feature_dim), nn.Tanh())
+        self.trunk = nn.Sequential(
+            nn.Linear(repr_dim, feature_dim), nn.LayerNorm(feature_dim), nn.Tanh()
+        )
 
         self.Q1 = nn.Sequential(
             nn.Linear(feature_dim + action_dim, hidden_dim),
-            nn.ReLU(inplace=True), nn.Linear(hidden_dim, hidden_dim),
-            nn.ReLU(inplace=True), nn.Linear(hidden_dim, 1))
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_dim, 1),
+        )
 
         self.Q2 = nn.Sequential(
             nn.Linear(feature_dim + action_dim, hidden_dim),
-            nn.ReLU(inplace=True), nn.Linear(hidden_dim, hidden_dim),
-            nn.ReLU(inplace=True), nn.Linear(hidden_dim, 1))
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_dim, 1),
+        )
 
         self.apply(utils.weight_init)
 
@@ -122,19 +130,21 @@ class Critic(nn.Module):
 
 
 class DrQV2Agent(StochasticAgent):
-    def __init__(self,
-                 obs_shape,
-                 action_dim,
-                 device,
-                 gamma,
-                 lr,
-                 feature_dim,
-                 hidden_dim,
-                 critic_target_tau,
-                 update_every_steps,
-                 stddev_schedule,
-                 stddev_clip,
-                 env_action_scaler):
+    def __init__(
+        self,
+        obs_shape,
+        action_dim,
+        device,
+        gamma,
+        lr,
+        feature_dim,
+        hidden_dim,
+        critic_target_tau,
+        update_every_steps,
+        stddev_schedule,
+        stddev_clip,
+        env_action_scaler,
+    ):
         self.device = device
         self.critic_target_tau = critic_target_tau
         self.update_every_steps = update_every_steps
@@ -144,15 +154,16 @@ class DrQV2Agent(StochasticAgent):
 
         # models
         encoder = Encoder(obs_shape).to(device)
-        actor = Actor(encoder.repr_dim, action_dim, feature_dim,
-                           hidden_dim).to(device)
+        actor = Actor(encoder.repr_dim, action_dim, feature_dim, hidden_dim).to(device)
         super().__init__(actor=actor, env_action_scaler=env_action_scaler)
 
         self.encoder = encoder
-        self.critic = Critic(self.encoder.repr_dim, action_dim, feature_dim,
-                             hidden_dim).to(device)
-        self.critic_target = Critic(self.encoder.repr_dim, action_dim,
-                                    feature_dim, hidden_dim).to(device)
+        self.critic = Critic(
+            self.encoder.repr_dim, action_dim, feature_dim, hidden_dim
+        ).to(device)
+        self.critic_target = Critic(
+            self.encoder.repr_dim, action_dim, feature_dim, hidden_dim
+        ).to(device)
         self.critic_target.load_state_dict(self.critic.state_dict())
         self.discount = gamma
 
@@ -209,14 +220,16 @@ class DrQV2Agent(StochasticAgent):
 
         # update critic
         metrics.update(
-            self.update_critic(obs, action, reward, discount, next_obs, env_step))
+            self.update_critic(obs, action, reward, discount, next_obs, env_step)
+        )
 
         # update actor
         metrics.update(self.update_actor(obs.detach(), env_step))
 
         # update critic target
-        utils.soft_update_params(self.critic, self.critic_target,
-                                 self.critic_target_tau)
+        utils.soft_update_params(
+            self.critic, self.critic_target, self.critic_target_tau
+        )
 
         return metrics
 
@@ -236,10 +249,10 @@ class DrQV2Agent(StochasticAgent):
         Q1, Q2 = self.critic(obs, action)
         critic_loss = F.mse_loss(Q1, target_Q) + F.mse_loss(Q2, target_Q)
 
-        metrics['critic_target_q'] = target_Q.mean().item()
-        metrics['critic_q1'] = Q1.mean().item()
-        metrics['critic_q2'] = Q2.mean().item()
-        metrics['critic_loss_mean'] = critic_loss.item() / 2
+        metrics["critic_target_q"] = target_Q.mean().item()
+        metrics["critic_q1"] = Q1.mean().item()
+        metrics["critic_q2"] = Q2.mean().item()
+        metrics["critic_loss_mean"] = critic_loss.item() / 2
 
         # optimize encoder and critic
         self.encoder_opt.zero_grad(set_to_none=True)
@@ -267,6 +280,6 @@ class DrQV2Agent(StochasticAgent):
         actor_loss.backward()
         self.actor_opt.step()
 
-        metrics['actor_loss'] = actor_loss.item()
+        metrics["actor_loss"] = actor_loss.item()
 
         return metrics
